aa371d2e0072e71cdffdf015f44410d51a842d80
felitba <feli@itba.edu.ar>
2024-10-21 18:14:30 -0300

    Update InversionStore.js

--- src/Stores/InversionStore.js
+++ src/Stores/InversionStore.js
@@ -1,1 +1,18 @@
-// ni idea
+// ni idea, userId, stockID?, amount?
+
+import { ref } from 'vue';
+import { defineStore } from 'pinia';
+//import {  } from '@/api/';
+
+export const useInversionStore = defineStore('inversion', () => {
+
+    const inversion = ref({
+        userId: '',
+        stockId: '',
+        amount: ''
+    })
+
+
+
+    //return {  }
+})
