80d62c3cebc59fee7c7aeb1266d4b6adb1b80592
Iker-Galante <igalante@itba.edu.ar>
2024-10-21 13:45:39 -0300

    Added Pinia

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -7,7 +7,8 @@
 // Plugins
 import vuetify from './vuetify'
 import router from '@/Router'
+import { createPinia } from 'pinia'
 
 export function registerPlugins (app) {
-  app.use(vuetify).use(router)
+  app.use(vuetify).use(router).use(createPinia())
 }
